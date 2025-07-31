ded00110156a7e3d119b6236aaa0a708a3ec5970
Yiping (Allison) <32723567+yiping-allison@users.noreply.github.com>
2025-07-31 14:14:47 -0700

    feat(fir): use oh-my-zsh
    wip

---
+++ home/dot_oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,14 @@
+# git
+alias gl='git log --graph --pretty=format:"%C(yellow)%H%Creset%nAuthor: %C(bold blue)%an <%ae>%Creset%nDate:   %C(green)%ad%Creset%n%n    %s%n" --date=iso'
+alias gs='git status'
+alias gap='git add -p'
+alias ga='git add .'
+alias gc='git commit'
+alias gco='git checkout'
+alias gd='git diff'
+alias gb='git branch'
+# git end
+
+# pnpm
+alias pn=pnpm
+# pnpm end
